944657f92e478e3e33001ac1098457f7e84ded85
Tejraj-singh <105737113+Tejraj-singh@users.noreply.github.com>
2022-05-18 19:28:47 +0530

    Update login_view.php

--- app/Views/login_view.php
+++ app/Views/login_view.php
@@ -1,13 +1,179 @@
-<?= $this->extend("base"); ?>
+<!DOCTYPE html>
 
+<head>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"><!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js"></script>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Login / Sign Up Form</title>
+    <link rel="shortcut icon" href="/assets/favicon.ico">
+    <style>
+body {background-color: #101522; }
+.hover-item{color:#f82249;}
+.hover-item:hover{background-color:white; color:#101522;}
+.hover-button{background-color:#f82249;}
+.hover-button:hover{background-color:#f82249; opacity: .6}
 
-<?= $this->section("content"); ?>
-<section id="hero">
-    <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
-      <h1 class="mb-4 pb-0">This is Login View</h1>
-      <p class="mb-4 pb-0">10-12 December, Downtown Conference Center, New York</p>
-      <a href="https://youtube.com/shorts/M9XoKPbz0-Y?feature=share" class="glightbox play-btn mb-4"></a>
-     
+</style>
+
+</head>
+
+<body>
+<div class="container w-50 p-5" >
+
+    <!-- Pills navs -->
+    <ul class="nav nav-pills nav-justified mb-3" id="ex1"  role="tablist">
+        <li class="nav-item"  role="presentation">
+            <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
+                aria-controls="pills-login" aria-selected="true">Login</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
+                aria-controls="pills-register" aria-selected="false">Register</a>
+        </li>
+    </ul>
+    <!-- Pills navs -->
+
+    <!-- Pills content -->
+    <div class="tab-content" >
+        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+            <form>
+                <div class="text-center mb-3">
+                    <p>Sign in with:</p>
+                    <button type="button"   class="btn btn-link btn-floating mx-1 hover-item">
+                        <i class="fab fa-facebook-f"></i>
+                    </button>
+
+                    <button type="button"  class=" btn btn-link btn-floating mx-1 hover-item ">
+                        <i class="fab fa-google"></i>
+                    </button>
+
+                    <button type="button" class="btn btn-link btn-floating mx-1 hover-item ">
+                        <i class="fab fa-twitter"></i>
+                    </button>
+
+                    <!-- <button type="button" class="btn btn-link btn-floating mx-1">
+                        <i class="fab fa-github"></i>
+                    </button> -->
+                </div>
+
+                <p class="text-center">or:</p>
+
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="loginName" class="form-control" />
+                    <label class="form-label" for="loginName">Email or username</label>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="loginPassword" class="form-control" />
+                    <label class="form-label" for="loginPassword">Password</label>
+                </div>
+
+                <!-- 2 column grid layout -->
+                <div class="row mb-4">
+                    <div class="col-md-6 d-flex justify-content-center">
+                        <!-- Checkbox -->
+                        <div class="form-check mb-3 mb-md-0">
+                            <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked />
+                            <label class="form-check-label" for="loginCheck"> Remember me </label>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 d-flex justify-content-center">
+                        <!-- Simple link -->
+                        <a href="#!">Forgot password?</a>
+                    </div>
+                </div>
+
+                <!-- Submit button -->
+                <button type="submit"  class="btn btn-primary btn-block mb-4 hover-button">Login</button>
+
+                <!-- Register buttons -->
+                <div class="text-center">
+                <p>Not a member?</p>
+
+                         <a  class="nav-link" href="#pills-register">Sign Up</a>
+                </div>
+            </form>
+        </div>
+        
+        <div class="tab-pane fade " id="pills-register" role="tabpanel" aria-labelledby="tab-register">
+            <form>
+                <div class="text-center mb-3">
+                    <p>Sign up with:</p>
+                    <button type="button"  class="btn btn-link btn-floating mx-1 hover-item ">
+                        <i class="fab fa-facebook-f"></i>
+                    </button>
+
+                    <button type="button"  class="btn btn-link btn-floating mx-1 hover-item ">
+                        <i class="fab fa-google"></i>
+                    </button>
+
+                    <button type="button"  class="btn btn-link btn-floating mx-1 hover-item ">
+                        <i class="fab fa-twitter"></i>
+                    </button>
+
+                    <!-- <button type="button" class="btn btn-link btn-floating mx-1">
+                        <i class="fab fa-github"></i>
+                    </button> -->
+                </div>
+
+                <p class="text-center">or:</p>
+
+                <!-- Name input -->
+                <!-- <div class="form-outline mb-4">
+                    <input type="text" id="registerName" class="form-control" />
+                    <label class="form-label" for="registerName">Name</label>
+                </div> -->
+
+                <!-- Username input -->
+                <div class="form-outline mb-4">
+                    <input type="text" id="registerUsername" class="form-control" />
+                    <label class="form-label" for="registerUsername">Username</label>
+                </div>
+
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="registerEmail" class="form-control" />
+                    <label class="form-label" for="registerEmail">Email</label>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="registerPassword" class="form-control" />
+                    <label class="form-label" for="registerPassword">Password</label>
+                </div>
+
+                <!-- Repeat Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="registerRepeatPassword" class="form-control" />
+                    <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+                </div>
+
+                <!-- Checkbox -->
+                <div class="form-check d-flex justify-content-center mb-4">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked
+                        aria-describedby="registerCheckHelpText" />
+                    <label class="form-check-label" for="registerCheck">
+                        I have read and agree to the terms
+                    </label>
+                </div>
+
+                <!-- Submit button -->
+                <button type="submit"  class=" btn btn-primary btn-block mb-3 hover-button">Sign Up</button>
+            </form>
+        </div>
     </div>
-  </section>
-<?= $this->endSection(); ?>
+    <!-- Pills content -->
+</div>
+</body>
