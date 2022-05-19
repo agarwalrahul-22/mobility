4dd5736b5618709477db77a7a1317c57a7f54328
aadesh610 <an30@iitbbs.ac.in>
2022-05-19 22:53:11 +0530

    updated

--- app/Views/login_view.php
+++ app/Views/login_view.php
@@ -20,7 +20,7 @@
     <title>Login / Sign Up Form</title>
     <link rel="shortcut icon" href="/assets/favicon.ico">
     <style>
-body { background-image: url("../../public/assets/img/svg11.png")}
+body { background-color:#101522;}
 .hover-item{color:#f82249;}
 .hover-item:hover{background-color:white; color:#101522;}
 .hover-button{background-color:#f82249;}
