cf032a3ae757aa9a66f61f3d5fbe02e02890e78c
Tejraj-singh <105737113+Tejraj-singh@users.noreply.github.com>
2022-05-19 16:34:47 +0530

    Tej

--- app/Views/login_view.php
+++ app/Views/login_view.php
@@ -1,3 +1,7 @@
+<?= $this->extend("base");?>
+
+
+<?= $this->section("content");?>
 <!DOCTYPE html>
 
 <head>
@@ -30,21 +34,12 @@ body {background-color: #101522; }
 <div class="container w-50 p-5" >
 
     <!-- Pills navs -->
-    <ul class="nav nav-pills nav-justified mb-3" id="ex1"  role="tablist">
-        <li class="nav-item"  role="presentation">
-            <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
-                aria-controls="pills-login" aria-selected="true">Login</a>
-        </li>
-        <li class="nav-item" role="presentation">
-            <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
-                aria-controls="pills-register" aria-selected="false">Register</a>
-        </li>
-    </ul>
+    
     <!-- Pills navs -->
 
     <!-- Pills content -->
-    <div class="tab-content" >
-        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+    <div  >
+        <div role="tabpanel" aria-labelledby="tab-login">
             <form>
                 <div class="text-center mb-3">
                     <p>Sign in with:</p>
@@ -101,79 +96,16 @@ body {background-color: #101522; }
                 <!-- Register buttons -->
                 <div class="text-center">
                 <p>Not a member?</p>
-
-                         <a  class="nav-link" href="#pills-register">Sign Up</a>
+                <a href="<?= base_url() ?>/Register">Sign Up</a>
+                         <!-- <a  class="nav-link" href="#pills-register">Sign Up</a> -->
                 </div>
             </form>
         </div>
         
-        <div class="tab-pane fade " id="pills-register" role="tabpanel" aria-labelledby="tab-register">
-            <form>
-                <div class="text-center mb-3">
-                    <p>Sign up with:</p>
-                    <button type="button"  class="btn btn-link btn-floating mx-1 hover-item ">
-                        <i class="fab fa-facebook-f"></i>
-                    </button>
-
-                    <button type="button"  class="btn btn-link btn-floating mx-1 hover-item ">
-                        <i class="fab fa-google"></i>
-                    </button>
-
-                    <button type="button"  class="btn btn-link btn-floating mx-1 hover-item ">
-                        <i class="fab fa-twitter"></i>
-                    </button>
-
-                    <!-- <button type="button" class="btn btn-link btn-floating mx-1">
-                        <i class="fab fa-github"></i>
-                    </button> -->
-                </div>
-
-                <p class="text-center">or:</p>
-
-                <!-- Name input -->
-                <!-- <div class="form-outline mb-4">
-                    <input type="text" id="registerName" class="form-control" />
-                    <label class="form-label" for="registerName">Name</label>
-                </div> -->
-
-                <!-- Username input -->
-                <div class="form-outline mb-4">
-                    <input type="text" id="registerUsername" class="form-control" />
-                    <label class="form-label" for="registerUsername">Username</label>
-                </div>
-
-                <!-- Email input -->
-                <div class="form-outline mb-4">
-                    <input type="email" id="registerEmail" class="form-control" />
-                    <label class="form-label" for="registerEmail">Email</label>
-                </div>
-
-                <!-- Password input -->
-                <div class="form-outline mb-4">
-                    <input type="password" id="registerPassword" class="form-control" />
-                    <label class="form-label" for="registerPassword">Password</label>
-                </div>
-
-                <!-- Repeat Password input -->
-                <div class="form-outline mb-4">
-                    <input type="password" id="registerRepeatPassword" class="form-control" />
-                    <label class="form-label" for="registerRepeatPassword">Repeat password</label>
-                </div>
-
-                <!-- Checkbox -->
-                <div class="form-check d-flex justify-content-center mb-4">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked
-                        aria-describedby="registerCheckHelpText" />
-                    <label class="form-check-label" for="registerCheck">
-                        I have read and agree to the terms
-                    </label>
-                </div>
-
-                <!-- Submit button -->
-                <button type="submit"  class=" btn btn-primary btn-block mb-3 hover-button">Sign Up</button>
-            </form>
-        </div>
+        
     </div>
     <!-- Pills content -->
 </div>
 </body>
+
+<?= $this->endSection(); ?>
