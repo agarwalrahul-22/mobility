16dcff4238c8126db54e6397942eaeff80a0b43e
Rishabhfelix <rkt10@iitbbs.ac.in>
2022-05-19 14:21:29 +0530

    updated about

--- app/Views/about_view.php
+++ app/Views/about_view.php
@@ -2,13 +2,56 @@
 
 
 <?= $this->section("content");?>
-  <section id="hero">
+ 
+  <!-- <section id="hero">
     <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
       <h1 class="mb-4 pb-0"> This is About View</h1>
       <p class="mb-4 pb-0">10-12 December, Downtown Conference Center, New York</p>
+       
       <a href="#more" class="about-btn scrollto">More</a>
     </div>
-  </section>
+  </section>  -->
+  <section class="devil">
+            <div class = "image">
+               <img src="<?=base_url()?>/public/assets/img/Egati.png">
+            </div>
+
+            <div class = "content">
+                <h2>About Us</h2>
+                <span><!-- line here --></span>
+                 <h3>Our company</h3>
+                <p>Electrogati is a company with a difference. Following are the characteristic features that make us distinct from the crowded space of other commercial organizations.</p>
+                
+                
+            </div>
+        </section>
+        <section>
+            <div class = "image">
+               <img src="<?=base_url()?>/public/assets/img/who we are.png">
+            </div>
+
+            <div class = "content">
+                
+                 <h4>Who we are</h4>
+                 <span><!-- line here --></span>
+                <p>Electrogati, founded in 2020 by some dreaming minds from the most premier Indian technical research institutes like IIT Bhubaneswar, IIT Hyderabad and IIT Delhi.Our team having specialization in the research area always thrive for Innovation.
+                     If you have problems… Then we try to provide you innovative solutions. We are here not for business alone; we are here for Bus-Res-Inn i.e. business integrated with research and innovation.</p>
+                
+            </div>
+        </section>
+        <section>
+            <div class = "image">
+               <img src="<?=base_url()?>/public/assets/img/istockphoto-854342964-612x612.jpg">
+            </div>
+
+            <div class = "content">
+                
+                 <h4>What we Value</h4>
+                 <span><!-- line here --></span>
+                <p>Innovation is our DNA, Creating value for you, not interested in profit alone, Premium, but cost conscious and we don’t sell a product, but sell a dream</p>
+                
+            </div>
+        </section>
 
 
 
