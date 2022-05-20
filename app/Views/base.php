61e380806503c1969b8e89026b11c4606f9f8852
Rishabhfelix <rkt10@iitbbs.ac.in>
2022-05-20 11:45:34 +0530

    adf

--- app/Views/base.php
+++ app/Views/base.php
@@ -7,9 +7,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- Template Main CSS File -->
-  <link href="<?= base_url() ?>/public/assets/css/style.css" rel="stylesheet">
+  
   <link href="<?= base_url() ?>/public/assets/css/styles.css" rel="stylesheet">
   <link href="<?= base_url() ?>/public/assets/css/contact.css" rel="stylesheet">
+  <link href="<?= base_url() ?>/public/assets/css/style.css" rel="stylesheet">
   <link rel="stylesheet" href="<?= base_url() ?>/public/assets/css/sale.css">
   <!-- Favicons -->
   <link href="<?= base_url() ?>/public/assets/img/favicon.png" rel="icon">
