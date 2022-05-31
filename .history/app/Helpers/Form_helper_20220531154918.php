784223da00542fbc747a2e5e278cb46aabb38e6b
agarwalrahul-22 <sj23@iitbbs.ac.in>
2022-05-31 16:27:23 +0530

    second

--- .history/app/Helpers/Form_helper_20220531154918.php
+++ .history/app/Helpers/Form_helper_20220531154918.php
@@ -3,10 +3,10 @@ function display_error($validation,$feild){
     if(isset($validation)){
         if($validation->hasError($feild))
         {
-
+            
             return $validation->getError($feild);
         }
-
+        
         else{
             return false;
         }
